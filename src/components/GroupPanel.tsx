116bd466d6d70bd7ca2d445fb1a5d77fc3240a6d
매능 <damiea987@gmail.com>
2025-12-20 10:10:30 +0900

    그룹 표기 디자인 변경 1인 근무 선택 로직 반영

--- src/components/GroupPanel.tsx
+++ src/components/GroupPanel.tsx
@@ -11,35 +11,18 @@ const GroupPanel = () => {
   return (
     <Flex flexDir={"column"} gap={2}>
       <Text fontWeight={"bold"}>조별 1인 근무횟수</Text>
-      <Flex gap={2}>
-        <Flex
-          p={2}
-          flexDir={"column"}
-          border={"1px solid black"}
-          borderRadius={4}
-        >
-          <Text fontWeight={"bold"}>주간</Text>
-          {dayGroup.map((count, idx) => (
-            <Flex gap={2}>
-              <Center p={2}>{GROUP_NAME[idx]}</Center>
-              <Center p={2}>{count}</Center>
-            </Flex>
-          ))}
-        </Flex>
-        <Flex
-          p={2}
-          flexDir={"column"}
-          border={"1px solid black"}
-          borderRadius={4}
-        >
-          <Text fontWeight={"bold"}>야간</Text>
-          {nightGroup.map((count, idx) => (
-            <Flex gap={2}>
-              <Center p={2}>{GROUP_NAME[idx]}</Center>
-              <Center p={2}>{count}</Center>
-            </Flex>
-          ))}
-        </Flex>
+      <Flex flexDir={"column"}>
+        {GROUP_NAME.map((name, idx) => (
+          <Flex gap={4} py={2}>
+            <Center fontWeight={"bold"}>{name}</Center>
+            <Center fontWeight={"bold"} color={"orange.300"}>
+              {dayGroup[idx]}
+            </Center>
+            <Center fontWeight={"bold"} color={"blue.300"}>
+              {nightGroup[idx]}
+            </Center>
+          </Flex>
+        ))}
       </Flex>
     </Flex>
   );
--- src/store/schedule.tsx
+++ src/store/schedule.tsx
@@ -365,7 +365,7 @@ const useSchedule = create<ScheduleStore>((set, get) => {
         group,
         dayWorkCount,
         nightWorkCount,
-        // aloneCount,
+        aloneCount,
       } = get();
 
       const sch = schedule.map((v) => [...v]);
@@ -374,10 +374,10 @@ const useSchedule = create<ScheduleStore>((set, get) => {
       const nGroup = [...nightGroup];
       const dWorkCount = [...dayWorkCount];
       const nWorkCount = [...nightWorkCount];
-      // const aCount = [...aloneCount];
+      const aCount = [...aloneCount];
 
       const dGroupIdx = (group + day) % 4;
-      const nGroupIdx = (group + day - 1) % 4;
+      const nGroupIdx = (group + day + 1) % 4;
 
       /* 변경 사항이 없을 경우*/
       if (sch[emp][day] == workType) return false;
@@ -389,14 +389,30 @@ const useSchedule = create<ScheduleStore>((set, get) => {
           dWorkCount[day]--;
           if (dWorkCount[day] === 0) {
             dGroup[dGroupIdx]--;
+            aCount[emp]--;
           }
-          if (dWorkCount[day] === 1) dGroup[dGroupIdx]++;
+          if (dWorkCount[day] === 1) {
+            dGroup[dGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 1,
+            );
+            if (target > -1) aCount[target]++;
+          }
         }
         if (sch[emp][day] == 2) {
           wk[emp].workCount--;
           nWorkCount[day]--;
-          if (nWorkCount[day] === 0) nGroup[nGroupIdx]--;
-          if (nWorkCount[day] === 1) nGroup[nGroupIdx]++;
+          if (nWorkCount[day] === 0) {
+            nGroup[nGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (nWorkCount[day] === 1) {
+            nGroup[nGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 2,
+            );
+            if (target > -1) aCount[target]++;
+          }
 
           if (day < numDays - 1) {
             sch[emp][day + 1] = 0;
@@ -415,8 +431,17 @@ const useSchedule = create<ScheduleStore>((set, get) => {
       if (workType === 1) {
         if (sch[emp][day] == 2) {
           nWorkCount[day]--;
-          if (nWorkCount[day] === 0) nGroup[nGroupIdx]--;
-          if (nWorkCount[day] === 1) nGroup[nGroupIdx]++;
+          if (nWorkCount[day] === 0) {
+            nGroup[nGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (nWorkCount[day] === 1) {
+            nGroup[nGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 2,
+            );
+            if (target > -1) aCount[target]++;
+          }
           if (day < numDays - 1) {
             sch[emp][day + 1] = 0;
             wk[emp].workCount--;
@@ -426,8 +451,17 @@ const useSchedule = create<ScheduleStore>((set, get) => {
         if (sch[emp][day] == 0 || sch[emp][day] == 5) wk[emp].workCount++;
         sch[emp][day] = 1;
         dWorkCount[day]++;
-        if (dWorkCount[day] === 1) dGroup[dGroupIdx]++;
-        if (dWorkCount[day] === 2) dGroup[dGroupIdx]--;
+        if (dWorkCount[day] === 1) {
+          dGroup[dGroupIdx]++;
+          aCount[emp]++;
+        }
+        if (dWorkCount[day] === 2) {
+          dGroup[dGroupIdx]--;
+          const target = worker.findIndex(
+            (_, idx) => idx != emp && sch[idx][day] == 1,
+          );
+          if (target > -1) aCount[target]--;
+        }
       }
       /* 야간으로 변경하는 경우 */
       if (workType === 2) {
@@ -436,8 +470,17 @@ const useSchedule = create<ScheduleStore>((set, get) => {
 
         if (sch[emp][day] == 1) {
           dWorkCount[day]--;
-          if (dWorkCount[day] === 0) dGroup[dGroupIdx]--;
-          if (dWorkCount[day] === 1) dGroup[dGroupIdx]++;
+          if (dWorkCount[day] === 0) {
+            dGroup[dGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (dWorkCount[day] === 1) {
+            dGroup[dGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 1,
+            );
+            if (target > -1) aCount[target]++;
+          }
         }
         if (sch[emp][day] == 3) if (day !== 0) return false;
 
@@ -447,8 +490,17 @@ const useSchedule = create<ScheduleStore>((set, get) => {
         }
         sch[emp][day] = 2;
         nWorkCount[day]++;
-        if (nWorkCount[day] === 1) nGroup[nGroupIdx]++;
-        if (nWorkCount[day] === 2) nGroup[nGroupIdx]--;
+        if (nWorkCount[day] === 1) {
+          nGroup[nGroupIdx]++;
+          aCount[emp]++;
+        }
+        if (nWorkCount[day] === 2) {
+          nGroup[nGroupIdx]--;
+          const target = worker.findIndex(
+            (_, idx) => idx != emp && sch[idx][day] == 2,
+          );
+          if (target > -1) aCount[target]--;
+        }
       }
       /* 비번으로 변경하는 경우 */
       if (workType === 3) {
@@ -457,15 +509,33 @@ const useSchedule = create<ScheduleStore>((set, get) => {
 
         if (sch[emp][day] === 1) {
           dWorkCount[day]--;
-          if (dWorkCount[day] === 0) dGroup[(day + 1) % 4]--;
-          if (dWorkCount[day] === 1) dGroup[(day + 1) % 4]++;
+          if (dWorkCount[day] === 0) {
+            dGroup[dGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (dWorkCount[day] === 1) {
+            dGroup[dGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 1,
+            );
+            if (target > -1) aCount[target]++;
+          }
         }
         if (sch[emp][day] == 2) {
           sch[emp][day + 1] = 0;
           wk[emp].workCount--;
           nWorkCount[day]--;
-          if (nWorkCount[day] === 0) nGroup[(day + 1) % 4]--;
-          if (nWorkCount[day] === 1) nGroup[(day + 1) % 4]++;
+          if (nWorkCount[day] === 0) {
+            nGroup[nGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (nWorkCount[day] === 1) {
+            nGroup[nGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 2,
+            );
+            if (target > -1) aCount[target]++;
+          }
         }
         sch[emp][day] = 3;
       }
@@ -474,13 +544,31 @@ const useSchedule = create<ScheduleStore>((set, get) => {
         if (sch[emp][day] == 0 || sch[emp][day] == 5) wk[emp].workCount++;
         if (sch[emp][day] === 1) {
           dWorkCount[day]--;
-          if (dWorkCount[day] === 0) dGroup[(day + 1) % 4]--;
-          if (dWorkCount[day] === 1) dGroup[(day + 1) % 4]++;
+          if (dWorkCount[day] === 0) {
+            dGroup[dGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (dWorkCount[day] === 1) {
+            dGroup[dGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 1,
+            );
+            if (target > -1) aCount[target]++;
+          }
         }
         if (sch[emp][day] == 2) {
           nWorkCount[day]--;
-          if (nWorkCount[day] === 0) nGroup[(day + 1) % 4]--;
-          if (nWorkCount[day] === 1) nGroup[(day + 1) % 4]++;
+          if (nWorkCount[day] === 0) {
+            nGroup[nGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (nWorkCount[day] === 1) {
+            nGroup[nGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 2,
+            );
+            if (target > -1) aCount[target]++;
+          }
           if (day < numDays - 1) {
             sch[emp][day + 1] = 0;
             wk[emp].workCount--;
@@ -494,13 +582,31 @@ const useSchedule = create<ScheduleStore>((set, get) => {
         if (sch[emp][day] == 1) {
           wk[emp].workCount--;
           dWorkCount[day]--;
-          if (dWorkCount[day] === 0) dGroup[dGroupIdx]--;
-          if (dWorkCount[day] === 1) dGroup[dGroupIdx]++;
+          if (dWorkCount[day] === 0) {
+            dGroup[dGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (dWorkCount[day] === 1) {
+            dGroup[dGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 1,
+            );
+            if (target > -1) aCount[target]++;
+          }
         }
         if (sch[emp][day] == 2) {
           nWorkCount[day]--;
-          if (nWorkCount[day] === 0) nGroup[(day + 1) % 4]--;
-          if (nWorkCount[day] === 1) nGroup[(day + 1) % 4]++;
+          if (nWorkCount[day] === 0) {
+            nGroup[nGroupIdx]--;
+            aCount[emp]--;
+          }
+          if (nWorkCount[day] === 1) {
+            nGroup[nGroupIdx]++;
+            const target = worker.findIndex(
+              (_, idx) => idx != emp && sch[idx][day] == 2,
+            );
+            if (target > -1) aCount[target]++;
+          }
           wk[emp].workCount--;
           if (day < numDays - 1) {
             sch[emp][day + 1] = 0;
@@ -521,6 +627,7 @@ const useSchedule = create<ScheduleStore>((set, get) => {
               nightGroup: nGroup,
               dayWorkCount: dWorkCount,
               nightWorkCount: nWorkCount,
+              aloneCount: aCount,
             }
           : {
               schedule: sch,
@@ -529,6 +636,7 @@ const useSchedule = create<ScheduleStore>((set, get) => {
               nightGroup: nGroup,
               dayWorkCount: dWorkCount,
               nightWorkCount: nWorkCount,
+              aloneCount: aCount,
             },
       );
       return true;
